45c2fa81fc29e5d0b9e3e69bffc7073dd099e8da
Aakash <aakashanand1995@gmail.com>
2023-12-25 12:35:57 +0530

    cleaned up the code for practice

--- schema/Users.js
+++ schema/Users.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
-const PLM = require(`passport-local-mongoose`);
+
 const userModel = new mongoose.Schema({
   username: {
     type: String,
@@ -15,7 +15,4 @@ const userModel = new mongoose.Schema({
     default: Date.now,
   },
 });
-
-userModel.plugin(PLM);
-
 module.exports = mongoose.model("UserCollection", userModel);
